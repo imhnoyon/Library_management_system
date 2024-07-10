66ea0afdb5db7f6277062685a1a555e7d4c1e0bb
ihmnoyon <noyonhasan320@gmail.com>
2024-07-10 19:06:12 +0600

    changes done

--- Library_Management_System/settings.py
+++ Library_Management_System/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
-
+CSRF_TRUSTED_ORIGINS = ['https://library-management-system-noyon.onrender.com']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
